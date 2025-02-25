7cc0e987be7bf434c10f944818549bf9bf12188f
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-02-25 11:52:13 -0300

    Update dotenv configuration to allow custom environment file path

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 import redis from 'redis';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
+dotenv.config({ path: process.env.ENV_FILE || ".env" });
 
 const redisHost = process.env.REDIS_HOST;
 const redisPort = process.env.REDIS_PORT;
