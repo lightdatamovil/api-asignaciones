4c2b0275e097baaefc4cc1609c4da55fc69a01ae
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-10-14 12:28:55 -0300

    Enhance logBlue output in getShipmentIdFromQr function to include complete shipment data in request completion message

--- src/functions/getShipmentIdFromQr.js
+++ src/functions/getShipmentIdFromQr.js
@@ -25,7 +25,7 @@ export async function getShipmentIdFromQr(companyId, dataQr) {
         const beforeRequest = performance.now();
         result = await axios.post("https://apimovil2.lightdata.app/api/qr/get-shipment-id", payload);
         const afterRequest = performance.now();
-        logBlue(`[getShipmentIdFromQr] Request completada en ${(afterRequest - beforeRequest).toFixed(2)} ms para el envio ${dataQr.id || dataQr.did} para la empresa ${companyId}`);
+        logBlue(`[getShipmentIdFromQr] Request completada en ${(afterRequest - beforeRequest).toFixed(2)} ms para el envio ${JSON.stringify(dataQr)} para la empresa ${companyId}`);
     } catch (error) {
         logRed(`[getShipmentIdFromQr] Error durante request: ${(performance.now() - startTime).toFixed(2)} ms`);
         throw new CustomException({
